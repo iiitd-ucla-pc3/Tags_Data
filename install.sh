1a54538f66d6d203597bb33416949ffc52770298
nipunreddevil <nipunreddevil@gmail.com>
2013-03-23 18:08:26 +0530

    Added install .sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+sudo pip install django
+sudo apt-get install python-mysqldb
+sudo apt-get install mysql-server mysql-client mysql-common
+
